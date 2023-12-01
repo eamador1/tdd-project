19d9b98b1d770d5a5ebc997ad658961b8aad1ffe
Erwin Amador <eamador0305@gmail.com>
2023-12-01 15:11:52 -0600

    give solution to challenge

--- solver.rb
+++ solver.rb
@@ -4,6 +4,12 @@ class Solver
   end
 
   def factorial(number)
-
+    if number.negative?
+      puts 'Input a zero or positive number'
+    elsif number.zero?
+      1
+    else
+      number * factorial(number - 1)
+    end
   end
 end
